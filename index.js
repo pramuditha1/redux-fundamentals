296208485d59bc516290544d644e1b82fd0885f8
pramuditha1 <pramuditha1@live.com>
2022-01-05 04:50:02 +0530

    root reducer & logger middleware implementation

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ redux store responsibilities
  - handles unregistering of listeners via the function returned by subscribe(listener)
 */
 const redux = require('redux')
-const createStore = redux.createStore
+const reduxLogger = require('redux-logger')
 
+const createStore = redux.createStore
+const combineReducers = redux.combineReducers
+
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 //action
 const BUY_CAKE = 'BUY_CAKE'
@@ -30,17 +35,27 @@ function buyIcecream(){
 //reducer
 // (previousState, action) => newState
 
-const initialState = {
-    numOfCakes:    10,
+const initialStateCake = {
+    numOfCakes:    10
+}
+
+const initialStateIceCream = {
     numOfIcecream: 20
 }
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialStateCake, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
         }//use spread operator: make a copy of state object and update only numOfCakes value in initial state object
+
+        default: return state
+    }
+}
+
+const iceCreamReducer = (state = initialStateIceCream, action) => {
+    switch(action.type) {
         case BUY_ICECREAM: return {
             ...state,
             numOfIcecream: state.numOfIcecream - 1
@@ -51,11 +66,21 @@ const reducer = (state = initialState, action) => {
 }
 
 //create store
-const store = createStore(reducer)
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+/* 
+after rootReducer parameter we apply a middleware
+What is the middleware it is logger. 
+provide a 3rd patry extension point between dispatching an action & the moment it reached the reducer. 
+we use muddleware for logging, crash reporting, performing asynchronous tasks etc...
+*/
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('Initial state', store.getState())
 
 //subscribe a listner to log the store value when store updated. everytime dispatch an action listner executes
-const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
